710cddffda6ca6f8a93eca3b4b54faa5f3ae5203
darkside <1759751014@qq.com>
2025-09-18 11:16:04 +0800

    file04

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,66 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+@Api(tags = "菜品接口")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    @PostMapping
+    @ApiOperation("保存菜品")
+    public Result save(@RequestBody DishDTO dishDTO) {
+        log.info("新增菜品:{}",dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("分页查询菜品:{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    @ApiOperation("批量删除菜品")
+    public Result delete(@RequestParam List<Long> ids) {
+        log.info("删除菜品:{}",ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("查询菜品ById")
+    public Result<DishVO> getById(@PathVariable Long id){
+        log.info("查询菜品ById:{}",id);
+        DishVO dishVO = dishService.getByIdWithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    @PutMapping
+    @ApiOperation("修改菜品")
+    public Result update(@RequestBody DishDTO dishDTO) {
+        log.info("修改菜品:{}",dishDTO);
+        dishService.updateWithFlavor(dishDTO);
+        return Result.success();
+    }
+}
